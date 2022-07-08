408d65a3155dfb24a7330c6a949e0990beea024f
Abhishek jha <46026348+sameerjha462000@users.noreply.github.com>
2022-07-08 11:47:18 +0530

    added removeLeaves at 49. . .

---
+++ 12genericTree/14removeLeaves.java
@@ -0,0 +1,82 @@
+import static java.lang.System.*;
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+import java.util.Stack;
+import java.util.Queue;
+
+// added remove leaves at 49...
+class node{
+	int data;
+	ArrayList<node> children;
+
+	node(int val){
+		data = val;
+		children = new ArrayList<>();
+	}
+
+	@Override
+	public String toString(){
+		String ans = "";
+		ans += data+"->";
+
+		for(node child : children)
+		{
+			ans += child.data + " ";
+		}
+
+		ans += "\n";
+
+		for(node child: children)
+		{
+			ans += child.toString();
+		}
+
+		return ans;
+	}
+}
+
+class Main{
+	public static void main(String[] args) {
+		initializeIO();
+		Integer[] vals = {10,20,50,null,60,null,null,30,70,null,80,110,
+						  null,120,null,null,90,null,null,40,100,null,null,null};
+
+		node root = constructGT(vals);
+		System.out.println(height(root));
+	}
+
+	public static void removeLeaves(node root)
+	{
+		int size = root.children.size()-1;
+
+		for(int i=size;i>=0;--i)
+		{
+			node child = root.children.get(i);
+
+			if(child.children.size() == 0)
+			{
+				root.children.remove(i);
+			}
+		}
+
+		for(node child : root.children)
+			removeLeaves(child);
+		if(root.children.size() == 0)
+			return;
+	}
+
+	private static void initializeIO(){
+        try 
+        {
+            System.setIn(new FileInputStream("input.txt"));
+            System.setOut(new PrintStream(new FileOutputStream("output.txt")));
+            System.setErr(new PrintStream(new FileOutputStream("error.txt")));
+        }
+        catch (Exception e) 
+        {
+            System.err.println(e.getMessage());
+        }
+    }
+
+}

4f645477d82f39698bd6cd6d42222893a76a4611
Abhishek jha <46026348+sameerjha462000@users.noreply.github.com>
2022-07-19 04:09:55 +0530

    BT

--- 13binaryTree/7inOrderIterative.java
+++ 13binaryTree/7inOrderIterative.java
@@ -1,31 +1,31 @@
     void inOrderIterative()
     {
-      Stack<node> st = new Stack<>();
-      st.push(root);
-
-      while(st.peek().left != null)
-      {
-        // keep pushing
-        st.push(st.peek().left);
-      }
-      while(!st.isEmpty())
-      {
-        while(st.peek().right == null)
-        {
-          node top = st.pop();
-          System.out.print(top.data + " ");
-        }
-
-        if(!st.isEmpty())
-        {
-          node topAgain = st.pop();
-          System.out.print(topAgain.data + " ");
-          st.push(topAgain.right);
+          Stack<node> st = new Stack<>();
+          st.push(root);
 
           while(st.peek().left != null)
           {
             st.push(st.peek().left);
           }
-        }
-      }
+        
+          while(!st.isEmpty())
+          {
+                while(st.peek().right == null)
+                {
+                  node top = st.pop();
+                  System.out.print(top.data + " ");
+                }
+
+                if(!st.isEmpty())
+                {
+                      node topAgain = st.pop();
+                      System.out.print(topAgain.data + " ");
+                      st.push(topAgain.right);
+
+                      while(st.peek().left != null)
+                      {
+                        st.push(st.peek().left);
+                      }
+                }
+          }
     }

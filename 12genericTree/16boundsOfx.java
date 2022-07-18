424e6434bb967ec792ee7ad0e3489c570748e713
Abhishek jha <46026348+sameerjha462000@users.noreply.github.com>
2022-07-19 04:29:45 +0530

    GT

--- 12genericTree/16boundsOfx.java
+++ 12genericTree/16boundsOfx.java
@@ -1,38 +1,3 @@
-import static java.lang.System.*;
-import java.lang.*;
-import java.util.*;
-import java.io.*;
-
-class node{
-	int data;
-	ArrayList<node> children;
-
-	node(int val){
-		data = val;
-		children = new ArrayList<>();
-	}
-
-	@Override
-	public String toString(){
-		String ans = "";
-		ans += data+"->";
-
-		for(node child : children)
-		{
-			ans += child.data + " ";
-		}
-
-		ans += "\n";
-
-		for(node child: children)
-		{
-			ans += child.toString();
-		}
-
-		return ans;
-	}
-}
-
 class pair{
 	int first;
 	int second;
@@ -48,8 +13,6 @@ class pair{
 		return "<"+first+", "+second+">";
 	}
 }
-
-// added bounds at 67...
 
 class Main{
 	public static void main(String[] args) {
@@ -89,42 +52,3 @@ class Main{
 		// if(root.children.size() == 0)
 		// 	return ;
 	}
-
-	public static node constructGT(Integer[] vals){
-
-		node root = new node(vals[0]);
-
-		Stack<node> st = new Stack<>();
-		st.push(root);
-
-		for(int i=1;i<vals.length;i++){
-			if(vals[i] == null){
-				st.pop();
-			}else{
-
-				// make a new node
-				node newNode = new node(vals[i]);
-
-				// make it children of the peek of the stack
-
-				st.peek().children.add(newNode);
-				st.push(newNode); 
-			}
-		}
-		return root;
-	}
-
-	private static void initializeIO(){
-        try 
-        {
-            System.setIn(new FileInputStream("input.txt"));
-            System.setOut(new PrintStream(new FileOutputStream("output.txt")));
-            System.setErr(new PrintStream(new FileOutputStream("error.txt")));
-        }
-        catch (Exception e) 
-        {
-            System.err.println(e.getMessage());
-        }
-    }
-
-}

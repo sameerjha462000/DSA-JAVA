d25e8b4449759e9bc207c8452f60f242dc9a8a04
Abhishek jha <46026348+sameerjha462000@users.noreply.github.com>
2022-07-19 04:27:49 +0530

    GT

--- 12genericTree/14removeLeaves.java
+++ 12genericTree/14removeLeaves.java
@@ -1,51 +1,3 @@
-import static java.lang.System.*;
-import java.lang.*;
-import java.util.*;
-import java.io.*;
-import java.util.Stack;
-import java.util.Queue;
-
-// added remove leaves at 49...
-class node{
-	int data;
-	ArrayList<node> children;
-
-	node(int val){
-		data = val;
-		children = new ArrayList<>();
-	}
-
-	@Override
-	public String toString(){
-		String ans = "";
-		ans += data+"->";
-
-		for(node child : children)
-		{
-			ans += child.data + " ";
-		}
-
-		ans += "\n";
-
-		for(node child: children)
-		{
-			ans += child.toString();
-		}
-
-		return ans;
-	}
-}
-
-class Main{
-	public static void main(String[] args) {
-		initializeIO();
-		Integer[] vals = {10,20,50,null,60,null,null,30,70,null,80,110,
-						  null,120,null,null,90,null,null,40,100,null,null,null};
-
-		node root = constructGT(vals);
-		removeLeaves(root);
-	}
-
 	public static void removeLeaves(node root)
 	{
 		int size = root.children.size()-1;
@@ -66,18 +18,3 @@ class Main{
 		if(root.children.size() == 0)
 			return;
 	}
-
-	private static void initializeIO(){
-        try 
-        {
-            System.setIn(new FileInputStream("input.txt"));
-            System.setOut(new PrintStream(new FileOutputStream("output.txt")));
-            System.setErr(new PrintStream(new FileOutputStream("error.txt")));
-        }
-        catch (Exception e) 
-        {
-            System.err.println(e.getMessage());
-        }
-    }
-
-}

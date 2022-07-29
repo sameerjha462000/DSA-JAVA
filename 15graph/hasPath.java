307002f100ded1a06caf4e97c9b0430ce0c393dd
Abhishek jha <46026348+sameerjha462000@users.noreply.github.com>
2022-07-30 00:17:36 +0530

    ㅤ

---
+++ 15graph/hasPath.java
@@ -0,0 +1,25 @@
+private static boolean hasPath(ArrayList<edge>[] graph, int src,int dest)
+    {
+        boolean[] vis = new boolean[graph.length];
+
+        return dfs(graph,src,dest,vis);
+    }
+
+    private static boolean dfs(ArrayList<edge>[] graph,int src,int dest,boolean[] vis)
+    {
+        vis[src] = true;
+
+        if(src == dest)
+            return true;
+
+        for(edge e : graph[src])
+        {
+            if(!vis[e.dest])
+            {
+                if(dfs(graph,e.dest,dest,vis))
+                    return true;
+            }
+        }
+
+        return false;
+    }
